e66c762a99d4cbc870f6e67379f5457284c3d1da
Ronnie Song <ronniesong0809@gmail.com>
2021-08-17 16:55:07 -0700

    configure webpack and babel

--- webpack.config.js
+++ webpack.config.js
@@ -1,43 +1,25 @@
-const path = require('path');
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const htmlPlugin = new HtmlWebPackPlugin({
+  template: "./src/index.html",
+  filename: "./index.html"
+});
+
 module.exports = {
-  mode: 'development',
-  entry: path.join(__dirname, 'src', 'index'),
-  watch: true,
-  output: {
-    path: path.join(__dirname, 'dist'),
-    publicPath: '/dist/',
-    filename: "bundle.js",
-    chunkFilename: '[name].js'
-  },
+  mode: "development",
   module: {
-    rules: [{
-      test: /.jsx?$/,
-      include: [
-        path.resolve(__dirname, 'src')
-      ],
-      exclude: [
-        path.resolve(__dirname, 'node_modules')
-      ],
-      loader: 'babel-loader',
-      query: {
-        presets: [
-          ["@babel/env", {
-            "targets": {
-              "browsers": "last 2 chrome versions"
-            }
-          }]
-        ]
+    rules: [
+      {
+        test: /.js?$/,
+        exclude: "/node_modules/",
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
       }
-    }]
+    ]
   },
-  resolve: {
-    extensions: ['.json', '.js', '.jsx']
-  },
-  devtool: 'source-map',
-  devServer: {
-    contentBase: path.join(__dirname, '/dist/'),
-    inline: true,
-    host: 'localhost',
-    port: 8080,
-  }
+  plugins: [htmlPlugin]
 };
